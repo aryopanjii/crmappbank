ece12dae48f88d46f6c3806d8e766a4954b3138b
aryopanjii <aryopanjii@gmail.com>
2023-09-24 15:16:01 +0700

    Update stream-bankchurn.py

--- stream-bankchurn.py
+++ stream-bankchurn.py
@@ -1,15 +1,9 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-
-st.title('Bank Customer Churn Prediction')
-credit_score = st.number_input('Credit Score')
-country = st.text_input('Country')
-gender = st.text_input('Gender')
-age = st.number_input('Age')
-tenure = st.number_input('Tenure')
-balance = st.number_input('Balance')
-products_number = st.number_input('Products Number')
-credit_card = st.number_input('Credit Card')
-active_member = st.number_input('Active Member')
-estimated_salary = st.number_input('Estimated Salary')
+st.title(“A Simple Streamlit Web App”)
+name = st.text_input(“Enter your name”, ‘’)
+st.write(f”Hello {name}!”)
+x = st.slider(“Select an integer x”, 0, 10, 1)
+y = st.slider(“Select an integer y”, 0, 10, 1)
+df = pd.DataFrame({“x”: [x], “y”: [y] , “x + y”: [x + y]}, index = [“addition row”])
+st.write(df)
