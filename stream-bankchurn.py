24056f58e064956ca5e27087ab7106faf9301b59
aryopanjii <aryopanjii@gmail.com>
2023-09-24 15:21:23 +0700

    Update stream-bankchurn.py

--- stream-bankchurn.py
+++ stream-bankchurn.py
@@ -1,6 +1,21 @@
 import streamlit as st
 import pandas as pd
 st.title("Bank Customer Churn Prediction")
+#test
+credit_score = st.number_input('Credit Score')
+country = st.text_input('Country')
+gender = st.text_input('Gender')
+age = st.number_input('Age')
+tenure = st.number_input('Tenure')
+balance = st.number_input('Balance')
+products_number = st.number_input('Products Number')
+credit_card = st.number_input('Credit Card')
+active_member = st.number_input('Active Member')
+estimated_salary = st.number_input('Estimated Salary')
+
+
+
+#tes
 name = st.text_input("Enter your name", '')
 st.write(f"Hello {name}!")
 x = st.slider("Select an integer x", 0, 10, 1)
