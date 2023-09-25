fd1ddd26e78e483f1f5db077bd83971605268e06
aryopanjii <aryopanjii@gmail.com>
2023-09-25 09:43:51 +0700

    Update stream-bankchurn.py

--- stream-bankchurn.py
+++ stream-bankchurn.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import pickle
+import sklearn
 
 # Load the Random Forest model from the pickle file
 model = pickle.load(open('randomforest.pkl', 'rb'))
