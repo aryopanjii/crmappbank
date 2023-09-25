01cf41b32f4d8ecc3ddd76d4e8718e06fcd5e823
aryopanjii <aryopanjii@gmail.com>
2023-09-25 09:15:38 +0700

    Update stream-bankchurn.py

--- stream-bankchurn.py
+++ stream-bankchurn.py
@@ -1,46 +1,45 @@
 import streamlit as st
-import pickle
 import numpy as np
-model  = pickle.load(open('clf.pkl','rb'))
-st.title('Bank Churn Prediction')
-html_temp ="""
-<div style ="background-color:#025246 ;padding:10px">
-<h2 style="color:white;text-align:center;">Bank Churn Prediction ML App</h2>
-</div>
-"""
-st.markdown(html_temp,unsafe_allow_html=True)
-Age =st.text_input("Age")
-Tenure =st.text_input("Tenure",)
-Balance =st.text_input("Balance")
-HasCrCard =st.text_input("HasCrCard")
-EstimatedSalary =st.text_input("EstimatedSalary")
+import pandas as pd
+import matplotlib.pyplot as plt
+import pickle
+import base64
+from sklearn.preprocessing import StandardScaler
 
-safe_html = """
-<div style ="background-color:#F4D03F;padding:10px>
-<h2 style="color:white; text-align:center;">Customer will not exit</h2>
-</div>
-"""
-danger_html="""
-<div style ="background-color:#F4D03F;padding:10px>
-<h2 style="color:white; text-align:center;">Customer will exit</h2>
-</div>
-"""
-def predict_cust(Age,	Tenure	,Balance,	HasCrCard,EstimatedSalary):
-    input = np.array([[Age,	Tenure	,Balance,	HasCrCard,EstimatedSalary]]).astype(np.float64)
-    prediction=model.predict_proba(input)
-    pred = np.argmax(prediction)
-    return pred
+st.write('''# *Client Churn Predictor*''')
+  
+uploaded_file = st.file_uploader("Upload a CSV file", type="csv")
+
+if uploaded_file:
+    input_df = pd.read_csv(uploaded_file)
+    st.write(
+    '''
+    ### Input Data ({} Customers)
+    '''.format(input_df.shape[0])
+    )
+    st.dataframe(input_df)
+    st.write('')
+    rfm = pickle.load( open( "../Project 3/ran_forest_mod.p", "rb" ) )
+
+    X = input_df.drop(labels = ['CustomerId'], axis = 1)
+
+    threshold = .22
+    y_preds = rfm.predict(X)
+    predicted_proba = rfm.predict_proba(X)
+    y_preds = (predicted_proba [:,1] >= threshold).astype('int')
+    op_list = []
+    for idx, exited in enumerate(y_preds):
+        if exited == 1:
+            op_list.append(input_df.CustomerId.iloc[idx])
+    st.write('''### Number of Potentially Churning Customers''')
+    st.write('''There are **{} customers** at risk of closing their accounts.'''.format(len(op_list)))
+
+    csv = pd.DataFrame(op_list).to_csv(index=False, header = False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    st.write('''''')
+    st.write('''''')
+    st.write('''### **⬇️ Download At-Risk Customer Id's**''')
+    href = f'<a href="data:file/csv;base64,{b64}" download="at_risk_customerids.csv">Download csv file</a>'
+    st.write(href, unsafe_allow_html=True)
     
-
-
-
-if st.button("Predict"):
-    output=predict_cust(Age,	Tenure	,Balance,	HasCrCard,EstimatedSalary)
-    st.success("The verdict{}".format(output))
-
-    if output ==0:
-        st.markdown(safe_html,unsafe_allow_html=True)
-    else:
-        st.markdown(danger_html,unsafe_allow_html=True)
-
-
+    
